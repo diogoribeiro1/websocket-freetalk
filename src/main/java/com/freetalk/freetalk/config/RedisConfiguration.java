c88eee846aa7db2cf85867973576a3da00cc70b2
Diogo Ribeiro <diogoribeiro698@gmail.com>
2025-03-28 00:57:52 -0300

    fix

--- src/main/java/com/freetalk/freetalk/config/RedisConfiguration.java
+++ src/main/java/com/freetalk/freetalk/config/RedisConfiguration.java
@@ -1,5 +1,7 @@
 package com.freetalk.freetalk.config;
 
+import java.time.Duration;
+
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -10,24 +12,33 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
-// @Slf4j
 @Configuration
 @EnableCaching
 @EnableScheduling
 public class RedisConfiguration {
-    public static final String NEWS_KEY = "news";
 
-    @CacheEvict(allEntries = true, value = NEWS_KEY)
+    public static final String LAST_MESSAGES_KEY = "lastMessages";
+    public static final String LAST_GROUP_MESSAGES_KEY = "lastGroupMessages";
+
+    @CacheEvict(allEntries = true, value = LAST_MESSAGES_KEY)
     @Scheduled(fixedDelayString = "${cache.ttl.ms:60000}")
-    public void evictCache() {
-        System.out.println("Cache cleared");
+    public void evictLastMessagesCache() {
+        System.out.println("Cache 'lastMessages' cleared");
+    }
+
+    @CacheEvict(allEntries = true, value = LAST_GROUP_MESSAGES_KEY)
+    @Scheduled(fixedDelayString = "${cache.ttl.ms:60000}")
+    public void evictLastGroupMessagesCache() {
+        System.out.println("Cache 'lastGroupMessages' cleared");
     }
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10)) // TTL de 10 minutos para cada cache
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                                new GenericJackson2JsonRedisSerializer()));
     }
 }
